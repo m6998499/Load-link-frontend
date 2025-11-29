ff94e1e813adf81f07a5294db450faa6737717fd
m6998499 <m6998499@gmail.com>
2025-11-29 11:39:10 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import "../styles.css";
+import '../styles.css';
+import Navbar from '../components/Navbar';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Navbar />
+      <div style={{ marginTop: "80px" }}>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
