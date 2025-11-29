61dce84d8283dcbe5d2073d14e583f5dc9cf0427
m6998499 <m6998499@gmail.com>
2025-11-29 10:53:58 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Load Link</h1>
+      <p>Welcome to the Load Link App.</p>
+      <a href="/login">Go to Login</a>
+    </div>
+  );
+}
