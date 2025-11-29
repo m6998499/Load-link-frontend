cbd8b8d5cd9d3c3b50c795424649fb717c94282d
m6998499 <m6998499@gmail.com>
2025-11-29 05:09:32 -0500

    Create index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>LoadLink Connect</h1>
+      <p>Your frontend is now working correctly!</p>
+    </div>
+  );
+}
